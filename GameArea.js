d12c4c5b0e0580b5683e4f93c6d8f56cb5302e13
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2022-03-28 10:46:38 +0000

    mappaAcqua

---
+++ GameArea.js
@@ -0,0 +1,17 @@
+export default class GameArea {
+    constructor() {
+      this.canvas = document.getElementById("gameArea");
+      this.canvas.width = 640;
+      this.canvas.height = 640;
+      this.context = this.canvas.getContext("2d");
+      this.interval = setInterval(this.updateGameArea, 20); //ogni 20 ms chiamo il metodo updateGameArea
+    }
+  
+    clear = () => {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+  
+    updateGameArea = () => {
+      this.clear();
+    };
+  }
---
+++ script.js
@@ -0,0 +1,4 @@
+import GameArea from "./GameArea";
+import Level1 from "./level1";
+import Levels from ".Levels"
+let myGameArea = new GameArea();
